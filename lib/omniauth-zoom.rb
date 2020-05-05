9b4bca228309f2e944e7c969be6252fd8d00be7d
Jason Charnes <jason@thecharnes.com>
2020-05-05 11:18:11 -0500

    Adds stategy so we can provide development credentials for review testing in production

--- lib/omniauth-zoom.rb
+++ lib/omniauth-zoom.rb
@@ -1,3 +1,4 @@
 require 'omniauth-zoom/version'
 require 'omniauth-zoom/api'
 require 'omniauth/strategies/zoom'
+require 'omniauth/strategies/zoom_review'
---
+++ lib/omniauth/strategies/zoom_review.rb
@@ -0,0 +1,56 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class ZoomReview < OmniAuth::Strategies::OAuth2
+      option :name, 'zoom_review'
+
+      option :client_options, {
+        token_url: "/oauth/token",
+        authorize_url: "/oauth/authorize",
+        site: "https://zoom.us"
+      }
+
+      uid { info['id'] }
+
+      info do
+        unless @info
+          api = OmniAuth::Zoom::API.new(token)
+          @info = api.get("/users/me")
+        end
+
+        @info
+      end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
+      def user_info
+        info
+      end
+
+      def token
+        access_token.token
+      end
+
+      credentials do
+        _credentials = {'token' => access_token.token}
+
+        if access_token.expires? && access_token.refresh_token
+          _credentials = _credentials.merge('refresh_token' => access_token.refresh_token)
+        end
+
+        if access_token.expires?
+          _credentials = _credentials.merge('expires_at' => access_token.expires_at)
+        end
+
+        _credentials.merge!('expires' => access_token.expires?)
+      end
+
+      extra do
+        { 'scope' => access_token.params['scope'] }
+      end
+    end
+  end
+end
